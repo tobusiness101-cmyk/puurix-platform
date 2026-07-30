b06c2c4004414750029f785dab4d2a1af0ccb586
tobusiness101-cmyk <to.business101@gmail.com>
2026-07-30 18:48:36 +0200

    robots

--- amersfoort-schoon-(1)/app/robots.ts
+++ amersfoort-schoon-(1)/app/robots.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
+      userAgent: '*', 
       allow: '/',
     },
     // Aangepast naar de URL mét www.
