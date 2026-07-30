7583d399b9df925e424c5b0634a92dc93ee7ad7c
tobusiness101-cmyk <to.business101@gmail.com>
2026-07-30 19:21:45 +0200

    layout

--- amersfoort-schoon-(1)/app/layout.tsx
+++ amersfoort-schoon-(1)/app/layout.tsx
@@ -20,6 +20,38 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        
+        {/* Gestructureerde Data voor Lokale SEO (Google Maps & Zoekresultaten) */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "LocalBusiness",
+              "name": "Puurix",
+              "image": "https://www.puurixschoonmaak.nl/logo.jpg",
+              "url": "https://www.puurixschoonmaak.nl",
+              "telephone": "+31624473102",
+              "email": "to.business101@gmail.com",
+              "areaServed": [
+                { "@type": "City", "name": "Oosterhout" },
+                { "@type": "City", "name": "Breda" },
+                { "@type": "City", "name": "Tilburg" },
+                { "@type": "City", "name": "Etten-Leur" },
+                { "@type": "City", "name": "Teteringen" },
+                { "@type": "City", "name": "Waalwijk" },
+                { "@type": "City", "name": "Kaatsheuvel" },
+                { "@type": "City", "name": "Amersfoort" }
+              ],
+              "address": {
+                "@type": "PostalAddress",
+                "addressRegion": "Noord-Brabant",
+                "addressCountry": "NL"
+              },
+              "priceRange": "€€"
+            }),
+          }}
+        />
       </body>
     </html>
   );
