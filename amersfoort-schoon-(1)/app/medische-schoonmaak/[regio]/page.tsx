b2d5c522a52ffd3f70c7192c39dff83d276c5440
tobusiness101-cmyk <to.business101@gmail.com>
2026-09-23 22:31:45 +0200

    l

--- amersfoort-schoon-(1)/app/medische-schoonmaak/[regio]/page.tsx
+++ amersfoort-schoon-(1)/app/medische-schoonmaak/[regio]/page.tsx
@@ -3,7 +3,6 @@ import { createRegioPage } from "@/lib/createRegioPage";
 const { generateStaticParams, generateMetadata, Component } = createRegioPage({
   serviceName: "Medische & Praktijkschoonmaak",
    basePath: "medische-schoonmaak",
-   basePath: "medische-schoonmaak",
 });
 
 export { generateStaticParams, generateMetadata };
