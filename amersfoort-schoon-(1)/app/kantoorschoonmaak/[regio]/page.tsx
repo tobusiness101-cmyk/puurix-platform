36c5e5822ecb25fffcf3400f37e63a19ebe33f2d
tobusiness101-cmyk <to.business101@gmail.com>
2026-09-23 22:16:12 +0200

    Update page.tsx

--- amersfoort-schoon-(1)/app/kantoorschoonmaak/[regio]/page.tsx
+++ amersfoort-schoon-(1)/app/kantoorschoonmaak/[regio]/page.tsx
@@ -2,6 +2,7 @@ import { createRegioPage } from "@/lib/createRegioPage";
 
 const { generateStaticParams, generateMetadata, Component } = createRegioPage({
   serviceName: "Kantoorschoonmaak",
+  basePath: "kantoorschoonmaak",
 });
 
 export { generateStaticParams, generateMetadata };
